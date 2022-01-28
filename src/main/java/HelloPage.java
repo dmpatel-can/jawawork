bc1e7c83aa119f9d7a71fc3b0748849c1e731eb2
dmpatel-can <dmpatel.can@gmail.com>
2022-01-27 18:51:15 -0600

    Updated Logon file

--- src/main/java/HelloPage.java
+++
@@ -1,8 +0,0 @@
-
-public class HelloPage
-{
-	public static void main(String[] args)
-	{
-		System.out.println("This is HelloPage of BDD");
-	}
-}
---
+++ src/main/java/LogOnPage.java
@@ -0,0 +1,9 @@
+
+public class LogOnPage
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is LogOnPage of BDD");
+		System.out.println("This is updated Page now");
+	}
+}
